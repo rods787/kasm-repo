b696fae86ebe015e5a41ef9e2fb3771d22ab717c
rods787 <respruel@liberty.edu>
2024-04-06 16:43:54 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-repo/1.0/',
+  basePath: '/kasm-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
